ca78908d7651625e1f26af685e577913a6e96bf0
Nathan Branco Caracho <nathan.caracho@hotmail.com>
2021-08-27 11:00:52 -0300

    create vue-press structure

---
+++ src/.vuepress/config.js
@@ -0,0 +1,76 @@
+const { description } = require('../../package')
+
+module.exports = {
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: 'JS+ Playbook',
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: description,
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ['meta', { name: 'theme-color', content: '#ff7d00' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    repo: '',
+    editLinks: false,
+    docsDir: 'docs',
+    editLinkText: '',
+    lastUpdated: true,
+    // overrideTheme: 'dark',
+    nav: [
+      {
+        text: 'Guide',
+        link: '/guide/',
+      },
+      {
+        text: 'Config',
+        link: '/config/'
+      },
+      {
+        text: 'VuePress',
+        link: 'https://v1.vuepress.vuejs.org'
+      }
+    ],
+    sidebar: {
+      '/': [
+        {
+          title: 'CSharp',
+          collapsable: true,
+          children: [
+            {
+              title: 'Melhores práticas',
+              collapsable: true,
+              children: [
+                'docs/csharp/best-practices/test',
+              ]
+            }
+          ]
+        }
+      ],
+    }
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+  ]
+}
---
+++ src/index.md
@@ -0,0 +1,3 @@
+<img src="https://apps-images-prd.s3.amazonaws.com/js%2B/logo.png" width="500px"/>
+
+# Playbook
